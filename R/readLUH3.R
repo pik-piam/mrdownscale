085d58aad012805c501216d1652d4c3d08716e29
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-01-08 16:51:13 +0100

    readLUH3 docs

--- R/readLUH3.R
+++ R/readLUH3.R
@@ -1,9 +1,10 @@
 #' readLUH3
 #'
 #' Read LUH3 data. For the states subtype, the secma and secmb categories are removed.
-#' For the management subtype, only the categories crpbf, rndwd, fulwd, fertl and irrig are read.
+#' For the management subtype, only the categories cpbf1, cpbf2, rndwd, fulwd, fertl and irrig are read.
 #' For the transitions subtype, only the wood harvest categories bioh and harv are read. To
 #' match magpie semantics years are shifted by 1 when reading transitions.
+#' The LUH3 nc files have day-based time, which is converted to years.
 #'
 #' @param subtype one of states, management, transitions, cellArea
 readLUH3 <- function(subtype) {
