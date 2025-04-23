65a642d98f6e58dcbb143824b60c6f7b5955bde8
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-04-23 15:29:04 +0200

    new constant cell size check calcLandTarget

--- R/calcLandTarget.R
+++ R/calcLandTarget.R
@@ -137,13 +137,22 @@ calcLandTarget <- function(target) {
   toolExpectTrue(setequal(sub("y[0-9]+\\.\\.", "", names(out)), expectedCategories),
                  "Land target categories match the corresponding mapping")
   toolExpectTrue(min(terra::minmax(out)) >= 0, "All values are >= 0")
-  totalAreas <- vapply(unique(terra::time(out)), function(year) {
-    sum(terra::values(out[[terra::time(out) == year]]), na.rm = TRUE)
-  }, double(1))
-  toolExpectLessDiff(max(totalAreas), min(totalAreas), 10^-4,
-                     "Total area is constant over time")
 
   years <- sort(unique(terra::time(out)))
+  totalAreas <- do.call(c, lapply(years, function(year) {
+    yearTotalArea <- terra::app(out[[terra::time(out) == year]], fun = "sum")
+    names(yearTotalArea) <- year
+    return(yearTotalArea)
+  }))
+  totalAreaDiff <- terra::app(totalAreas, fun = "max") - terra::app(totalAreas, fun = "min")
+  minmax <- terra::minmax(totalAreaDiff, compute = TRUE)
+  stopifnot(minmax["min", ] >= 0)
+  threshold <- 10^-7
+  toolExpectTrue(minmax["max", ] < threshold,
+                 paste0("Total area of each cell is constant over time ",
+                        "(maxdiff = ", signif(minmax["max", ], 3),
+                        ", threshold = ", threshold, ")"))
+
   primfn <- out["primf|primn"]
   primfnTime <- terra::time(primfn)
   primfnDiff <- primfn[[primfnTime %in% years[-1]]] - primfn[[primfnTime %in% years[-length(years)]]]
