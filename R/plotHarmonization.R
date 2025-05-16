0e5809d65472bdbbdbe2cfd4f960d0279da91990
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-05-16 14:31:18 +0200

    histogram

--- R/plotHarmonization.R
+++ R/plotHarmonization.R
@@ -29,3 +29,41 @@ plotHarmonization <- function(input, target, harmonizationPeriod) {
           legend.title = element_text(size = fontsize)) +
     scale_y_continuous(n.breaks = 8)
 }
+
+plotHistogram <- function(input, target, harmonizationPeriod, variables) {
+  # e.g. crops: variables <- grep("^c[34](ann|per|nfx)", getItems(lir, 3), value = TRUE)
+  lir <- calcOutput("LandInputRecategorized", input = input, target = target, aggregate = FALSE)
+  ltex <- calcOutput("LandTargetExtrapolated", input = input, target = target,
+                     harmonizationPeriod = harmonizationPeriod, aggregate = FALSE)
+  lh <- calcOutput("LandHarmonized", aggregate = FALSE, input = input,
+                   target = target, harmonizationPeriod = harmonizationPeriod)
+
+  years <- getYears(lh, TRUE)
+  years <- years[years >= harmonizationPeriod[[1]] & years < harmonizationPeriod[[2]]]
+
+  lirCrop <- dimSums(lir[, years, variables], 3)
+  ltexCrop <- dimSums(ltex[, years, variables], 3)
+  lhCrop <- dimSums(lh[, years, variables], 3)
+
+  x <- lirCrop - lhCrop
+  x <- x / dimSums(lir[, years, ], 3)
+
+  x <- as.data.frame(x)
+  # x <- x[x$Region == "EUR", ]
+  names(x)[4:5] <- c("Data", "RelativeDiff")
+  library(ggplot2)
+  fontsize <- 25
+  (ggplot(data = x, aes(RelativeDiff, group = Region))
+    + ggplot2::geom_histogram()
+    + facet_grid(vars(Year), vars(Region))
+    # + facet_wrap(~Year)
+    + theme(text = element_text(size = fontsize),
+            axis.text = element_text(size = fontsize),
+            axis.title = element_text(size = fontsize),
+            plot.title = element_text(size = fontsize),
+            legend.text = element_text(size = fontsize),
+            legend.title = element_text(size = fontsize))
+    # + scale_y_continuous(n.breaks = 5)
+    # + scale_x_continuous(n.breaks = 8)
+  )
+}
