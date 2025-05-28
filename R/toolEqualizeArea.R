1c3a5252db54738ac6e42c8007e83d44bf3e3dc9
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-05-28 16:27:26 +0200

    handle edge case equalizeArea

--- R/toolEqualizeArea.R
+++ R/toolEqualizeArea.R
@@ -4,6 +4,7 @@ toolEqualizeArea <- function(x, y, level = 1) {
   ySum <- dimSums(y, dim = 3)
   if (max(abs(xSum - ySum)) >= 10^-5) {
     corr <- setYears(ySum / xSum, NULL)
+    corr[ySum == 0 & xSum == 0] <- 1
     stopifnot(is.finite(corr), corr >= 0)
     toolStatusMessage("note", paste0("correction factors were applied to equalize area ",
                                      "(max ratio = ", round(max(corr), 2),
