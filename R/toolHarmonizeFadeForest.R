c9396917ca7016b1adc1be38a1e3962644bb855e
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-06-05 11:06:09 +0200

    minimize primf to secdf conversion

--- R/toolHarmonizeFadeForest.R
+++ R/toolHarmonizeFadeForest.R
@@ -1,8 +1,7 @@
 #' toolHarmonizeFadeForest
 #'
-#' Harmonize using \code{\link{toolHarmonizeFade}}, then sum up primf + secdf
-#' and disaggregate according to xTarget weights, in order to avoid unintended
-#' conversion from primf to secdf.
+#' Harmonize using \code{\link{toolHarmonizeFade}}, then sum up primf + secdf,
+#' then disaggregate again in a way that minimizes primf to secdf conversion.
 #'
 #' @inheritParams toolHarmonizeFade
 #' @inherit toolHarmonizeFade return
@@ -12,37 +11,20 @@ toolHarmonizeFadeForest <- function(xInput, xTarget, harmonizationPeriod) {
   x <- toolHarmonizeFade(xInput, xTarget, harmonizationPeriod, level = 4)
   years <- getYears(x, as.integer = TRUE)
   psf <- c("primf", "secdf")
-  forest <- dimSums(x[, years > hp1, psf], 3)
-  shares <- xTarget[, hp1, psf] / dimSums(xTarget[, hp1, psf], 3)
-  shares[is.nan(shares)] <- 1 / length(psf)
+  forest <- dimSums(x[, years >= hp1, psf], 3)
 
-  # lowest total forest stock per cluster/region
-  low <- magpply(forest, min, DIM = 2)
+  primf <- xTarget[, hp1, "primf"]
+  for (year in years[years > hp1]) {
+    previousYear <- primf[, nyears(primf), ]
+    p <- mpmin(previousYear, forest[, year, ])
+    p <- setYears(p, year)
+    primf <- mbind(primf, p)
+  }
+  primsecdf <- mbind(primf,
+                     magclass::setNames(forest - primf, "secdf"))
 
-  primf <- mpmin(mpmin(forest[, max(years), ] * collapseDim(shares)[, , "primf"],
-                       xTarget[, hp1, "primf"]),
-                 low)
-
-  # interpolation is not linear :(
-  primf <- time_interpolate(mbind(xTarget[, hp1, "primf"],
-                                  primf),
-                            years[years > hp1])
-  # convergence(xTarget[, hp1, "primf"],
-  #             primf,
-  #             hp1,
-  #             max(years),
-  #             type = "linear")
-
-  secdf <- setNames(forest - primf, "secdf")
-  negativeSecdf <- secdf
-  negativeSecdf[negativeSecdf > 0] <- 0
-  primf <- primf + collapseDim(negativeSecdf)
-  secdf[secdf < 0] <- 0
-
-  primsecdf <- mbind(primf, secdf)
-  stopifnot(abs(forest - dimSums(primsecdf, 3)) < 10^-10)
-  x[, years > hp1, psf] <- primsecdf
-  browser()
-  # x <- toolReplaceExpansion(x, "primf", "secdf", warnThreshold = 100, level = 3)
+  stopifnot(primsecdf >= 0,
+            abs(forest - dimSums(primsecdf, 3)) < 10^-10)
+  x[, years >= hp1, psf] <- primsecdf
   return(x)
 }
