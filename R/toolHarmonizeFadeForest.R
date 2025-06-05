a08ef2736f10a5e5b4fded3f6572a4e3f6d3422f
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-06-05 10:27:03 +0200

    WIP not working

--- R/toolHarmonizeFadeForest.R
+++ R/toolHarmonizeFadeForest.R
@@ -15,9 +15,34 @@ toolHarmonizeFadeForest <- function(xInput, xTarget, harmonizationPeriod) {
   forest <- dimSums(x[, years > hp1, psf], 3)
   shares <- xTarget[, hp1, psf] / dimSums(xTarget[, hp1, psf], 3)
   shares[is.nan(shares)] <- 1 / length(psf)
-  primsecdf <- forest * collapseDim(shares)
+
+  # lowest total forest stock per cluster/region
+  low <- magpply(forest, min, DIM = 2)
+
+  primf <- mpmin(mpmin(forest[, max(years), ] * collapseDim(shares)[, , "primf"],
+                       xTarget[, hp1, "primf"]),
+                 low)
+
+  # interpolation is not linear :(
+  primf <- time_interpolate(mbind(xTarget[, hp1, "primf"],
+                                  primf),
+                            years[years > hp1])
+  # convergence(xTarget[, hp1, "primf"],
+  #             primf,
+  #             hp1,
+  #             max(years),
+  #             type = "linear")
+
+  secdf <- setNames(forest - primf, "secdf")
+  negativeSecdf <- secdf
+  negativeSecdf[negativeSecdf > 0] <- 0
+  primf <- primf + collapseDim(negativeSecdf)
+  secdf[secdf < 0] <- 0
+
+  primsecdf <- mbind(primf, secdf)
   stopifnot(abs(forest - dimSums(primsecdf, 3)) < 10^-10)
   x[, years > hp1, psf] <- primsecdf
-  x <- toolReplaceExpansion(x, "primf", "secdf", warnThreshold = 100, level = 3)
+  browser()
+  # x <- toolReplaceExpansion(x, "primf", "secdf", warnThreshold = 100, level = 3)
   return(x)
 }
