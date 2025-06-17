7c6f5a762922c7fa04edd804928b6d4e6447104b
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-06-17 11:47:59 +0200

    works

--- R/toolHarmonizeFadeForest.R
+++ R/toolHarmonizeFadeForest.R
@@ -7,19 +7,31 @@
 #' @inherit toolHarmonizeFade return
 #' @author Pascal Sauer
 toolHarmonizeFadeForest <- function(xInput, xTarget, harmonizationPeriod) {
+  hp1 <- harmonizationPeriod[1]
+
   x <- toolHarmonizeFade(xInput, xTarget, harmonizationPeriod, level = 4)
   years <- getYears(x, as.integer = TRUE)
   psf <- c("primf", "secdf")
   if (!all(psf %in% getItems(x, 3))) {
     return(x)
   }
-  hp1 <- harmonizationPeriod[1]
   forest <- dimSums(x[, years >= hp1, psf], 3)
+
+
+  a <- xTarget[, getYears(xTarget, TRUE) <= hp1, "primf"]
+  di <- -a[, -1, ] + setYears(a[, -nyears(a), ],
+                              getYears(a[, -1, ]))
+  meanPrimfDecline <- magpply(di, mean, DIM = 2)
+  b <- xTarget[, hp1, "primf"]
+  for (year in years[years > hp1]) {
+    b <- mbind(b, setYears(b[, nyears(b), ] - meanPrimfDecline, year))
+  }
+  b[b < 0] <- 0
 
   primf <- xTarget[, hp1, "primf"]
   for (year in years[years > hp1]) {
-    previousYear <- primf[, nyears(primf), ]
-    p <- mpmin(previousYear, forest[, year, ])
+    p <- mpmin(b[, year, ], forest[, year, ])
+    p <- mpmin(p, primf[, nyears(primf), ])
     p <- setYears(p, year)
     primf <- mbind(primf, p)
   }
