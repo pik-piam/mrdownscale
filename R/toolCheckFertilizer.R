92705f11304e123b2da581a6f5724307a6ba77a2
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-07-08 12:45:54 +0200

    +toolFertilizerKgPerHa

--- R/toolCheckFertilizer.R
+++ R/toolCheckFertilizer.R
@@ -1,10 +1,15 @@
 toolCheckFertilizer <- function(fertilizerTg, landMha, threshold = 1200) {
-  cropMha <- toolAggregateCropland(landMha, getItems(fertilizerTg, 3.2), keepOthers = FALSE)
-  # convert from Tg yr-1 to kg ha-1 yr-1
-  fertilizerKgPerHa <- fertilizerTg / cropMha * (10^9 / 10^6)
-  fertilizerKgPerHa[is.nan(fertilizerKgPerHa)] <- 0
+  fertilizerKgPerHa <- toolFertilizerKgPerHa(fertilizerTg, landMha)
   toolExpectTrue(max(fertilizerKgPerHa) <= threshold,
                  paste0("Fertilizer application is <= ", threshold, " kg ha-1 yr-1 (max: ",
                         signif(max(fertilizerKgPerHa), 3), ")"),
                  level = 1)
+}
+
+toolFertilizerKgPerHa <- function(fertilizerTg, landMha) {
+  cropMha <- toolAggregateCropland(landMha, getItems(fertilizerTg, 3.2), keepOthers = FALSE)
+  # convert from Tg yr-1 to kg ha-1 yr-1
+  fertilizerKgPerHa <- fertilizerTg / cropMha * (10^9 / 10^6)
+  fertilizerKgPerHa[is.nan(fertilizerKgPerHa)] <- 0
+  return(fertilizerKgPerHa)
 }
