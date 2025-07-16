6b3cedafecb326bfaaf8c61096874d87e6a9a1ff
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-07-16 14:15:25 +0200

    documentation toolAggregateCropland

--- R/toolAggregateCropland.R
+++ R/toolAggregateCropland.R
@@ -1,3 +1,14 @@
+#' toolAggregateCropland
+#'
+#' Aggregate variables like c3ann_irrigated and c3ann_rainfed to just c3ann.
+#'
+#' @param land magpie object with variables starting with cropTypes in dim 3
+#' @param cropTypes character vector, the variables to aggregate to
+#' @param ... reserved for future expansion
+#' @param keepOthers logical, if FALSE cropTypes will be the only variables in output
+#' @return land with crop data aggregated to cropTypes
+#'
+#' @author Pascal Sauer
 toolAggregateCropland <- function(land,
                                   cropTypes = c("c3ann", "c4ann", "c3per", "c4per", "c3nfx"),
                                   ...,
