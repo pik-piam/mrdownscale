40dad19bf0ac983fb2c7a1ea9e9caf1afa551aca
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-07-28 15:02:26 +0200

    readWITCH

--- R/readWITCH.R
+++ R/readWITCH.R
@@ -1,3 +1,11 @@
 readWITCH <- function() {
-  # TODO
+  if (!requireNamespace("nanoparquet", quietly = TRUE)) {
+    stop("Please install nanoparquet, e.g. with install.packages('nanoparquet')")
+  }
+  x <- nanoparquet::read_parquet("db_ssp2_M_luh3.parquet")
+  unique(x$units)
+  return(list(x = x,
+              class = "data.frame",
+              unit = paste(unique(x$units), collapse = ", "),
+              description = "WITCH data"))
 }
