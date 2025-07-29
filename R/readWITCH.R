af07c3fc6d7f7cb6c5399162a3cfbd9f686bf07d
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-07-29 10:42:16 +0200

    read csv instead of parquet

--- R/readWITCH.R
+++ R/readWITCH.R
@@ -1,9 +1,5 @@
 readWITCH <- function() {
-  if (!requireNamespace("nanoparquet", quietly = TRUE)) {
-    stop("To read WITCH data please install nanoparquet, e.g. with install.packages('nanoparquet')")
-  }
-  x <- nanoparquet::read_parquet("db_ssp2_M_luh3.parquet")
-  x$region <- levels(x$region)[x$region]
+  x <- utils::read.csv("db_ssp2_M_luh3.csv")
   return(list(x = x,
               class = "data.frame",
               unit = paste(unique(x$units), collapse = ", "),
