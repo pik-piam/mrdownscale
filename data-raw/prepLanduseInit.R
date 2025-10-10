98c665432767562b716438590445789124f58509
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-10-10 15:24:25 +0200

    prepLanduseInit

---
+++ data-raw/prepLanduseInit.R
@@ -0,0 +1,6 @@
+library(mrmagpie)
+x <- calcOutput("LanduseInitialisation", nclasses = "seven",
+                aggregate = FALSE, cellular = TRUE, cells = "lpjcell",
+                input_magpie = TRUE)
+x <- x[, seq(1995, 2020, 5), ]
+write.magpie(x, "avl_land_t_0.5.mz")
