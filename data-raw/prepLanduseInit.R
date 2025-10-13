2b11f485b766b946b680f786a41b376559a8da7e
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-10-13 14:58:14 +0200

    export toolReplaceExpansion, build

--- data-raw/prepLanduseInit.R
+++ data-raw/prepLanduseInit.R
@@ -1,6 +1,7 @@
-library(mrmagpie)
-x <- calcOutput("LanduseInitialisation", nclasses = "seven",
-                aggregate = FALSE, cellular = TRUE, cells = "lpjcell",
-                input_magpie = TRUE)
-x <- x[, seq(1995, 2020, 5), ]
-write.magpie(x, "avl_land_t_0.5.mz")
+withr::with_package("mrmagpie", {
+  x <- calcOutput("LanduseInitialisation", nclasses = "seven",
+                  aggregate = FALSE, cellular = TRUE, cells = "lpjcell",
+                  input_magpie = TRUE)
+  x <- x[, seq(1995, 2020, 5), ]
+  write.magpie(x, "avl_land_t_0.5.mz")
+})
