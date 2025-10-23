bfdac957c0e8901f30a9febc2c831c43dc3113d4
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-10-23 14:53:19 +0200

    remove setequalDims, use sameDims instead

--- R/setequalDims.R
+++
@@ -1,5 +0,0 @@
-setequalDims <- function(a, b) {
-  return(setequal(getItems(a, 1), getItems(b, 1)) &&
-           setequal(getItems(a, 2), getItems(b, 2)) &&
-           setequal(getItems(a, 3), getItems(b, 3)))
-}
