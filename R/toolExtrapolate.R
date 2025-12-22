e282e19cf051f2e45ed5ffcb5aa6f1b4f63f0cc6
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-12-22 11:16:18 +0100

    remove unused args: linearModel, fallback

--- R/toolExtrapolate.R
+++ R/toolExtrapolate.R
@@ -2,23 +2,18 @@
 #'
 #' Extrapolate a dataset into the future. A linear model is fitted for
 #' each combination of spatial entity and category, and then used to predict
-#' the value of the last requested extrapolation year. A spline-based
-#' interpolation ensures a smooth transition from historical values to the
-#' predicted value. If the linear model is not significant (p > 0.05) or
-#' linearModel is FALSE, the same value is used for all years instead
-#' (constant extrapolation, see argument called fallback).
+#' the value of the last requested extrapolation year. If the linear model
+#' is not significant (p > 0.05) the historical mean is used instead.
+#' A spline-based interpolation is used to create a smooth transition from
+#' historical values to the predicted value.
 #'
 #' @param x A magpie object with "year" as the temporal dimension and
 #' without any NAs
 #' @param years A vector of years to extrapolate to
-#' @param linearModel If TRUE, a linear model is fitted for each
-#' combination of spatial entity and category.
-#' @param fallback What constant value to use for all years if the linear
-#' model is disabled or not significant. Either "mean" or "last". If "mean",
-#' use the mean value of x. If "last", use the value of the last year in x.
 #' @return A magpie object like x but with the extrapolated years only
+#'
 #' @author Pascal Sauer
-toolExtrapolate <- function(x, years, linearModel = TRUE, fallback = "mean") {
+toolExtrapolate <- function(x, years) {
   stopifnot(names(dimnames(x))[2] == "year",
             !is.na(x),
             !years %in% getYears(x, as.integer = TRUE))
@@ -28,18 +23,12 @@ toolExtrapolate <- function(x, years, linearModel = TRUE, fallback = "mean") {
   return(do.call(mbind, lapply(getItems(x, 1), function(location) {
     return(do.call(mbind, lapply(getItems(x, 3), function(category) {
       d <- as.data.frame(x[location, , category], rev = 3)
+      fallbackValue <- mean(d$.value)
       prediction <- d[rep(1, length(years)), ]
       prediction$year <- years
-      if (fallback == "mean") {
-        fallbackValue <- mean(d$.value)
-      } else if (fallback == "last") {
-        fallbackValue <- d[d$year == max(d$year), ".value"]
-      } else {
-        stop("Invalid fallback method")
-      }
       prediction$.value <- fallbackValue
 
-      if (!all(d$.value == d$.value[1]) && linearModel) {
+      if (!all(d$.value == d$.value[1])) {
         model <- stats::lm(.value ~ year, data = d)
 
         # predict last year using linear model if significant, fallback otherwise
